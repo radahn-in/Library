5abd68c66046794266a8fb8b93d4a5c1716dabd8
radahn-in <raghu_nath1993@yahoo.com>
2025-03-02 18:35:58 +0530

    display function for library

--- book.js
+++ book.js
@@ -8,25 +8,45 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  let title = document.getElementById("#title")
-  let author = document.getElementById("#author")
-  let pages = document.getElementById("#pages")
-  let read = document.getElementById("#read").checked;
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
   let newBook = new Book(title, author, pages, read)
   myLibrary.push(newBook);
+  display();
 }
+function display() {
+  let libraryEl = document.querySelector(".library");
+  libraryEl.innerHTML = "";
 
-/*const dataElements = document.querySelector('.library');
+  myLibrary.forEach((book, index) => {
+    let bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
 
-myLibrary.map(book => {
-  dataElements.insertAdjacentHTML('afterbegin', `
-    
-`)
-})*/
+    bookDiv.innerHTML = `
+      <p><strong>Title:</strong> ${book.title}</p>
+      <p><strong>Author:</strong> ${book.author}</p>
+      <p><strong>Pages:</strong> ${book.pages}</p>
+      <p><strong>Read:</strong> ${book.read ? "Yes" : "No"}</p>
+      <button onclick="removeBook(${index})">Remove</button>
+        `;
+    libraryEl.appendChild(bookDiv);
+  })
+}
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  display();
+}
 
 let newBookbtn = document.querySelector('#new-book-btn');
 newBookbtn.addEventListener('click', function () {
   let bookForm = document.querySelector('#book-form');
   bookForm.style.display = "block"
+});
+
+document.querySelector('#add-book-btn').addEventListener("click", function (e) {
+  e.preventDefault();
+  addBookToLibrary();
 })
 
