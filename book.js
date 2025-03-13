0ba57c0b653f1e74d92f4d22545bd795ff4417e7
radahn-in <raghu_nath1993@yahoo.com>
2025-03-13 06:59:43 +0530

    refactoring to classes

--- book.js
+++ book.js
@@ -1,20 +1,54 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
-function addBookToLibrary() {
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
-  let read = document.getElementById("read").checked;
-  let newBook = new Book(title, author, pages, read)
-  myLibrary.push(newBook);
-  display();
+class Library {
+  constructor() {
+    this.books = [];
+    this.initEventListeners();
+  }
+
+  addBook() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+
+    const newBook = new Book(title, author, pages, read);
+    this.books.push(newBook)
+    this.display();
+  }
+  removeBook(index) {
+    this.books.splice(index, 1);
+    this.display();
+  }
+
+  display() {
+    const libraryEl = document.querySelector('.library');
+    libraryEl.innerHTML = "";
+
+    this.books.forEach((book, index) => {
+      const bookDiv = document.createElement('div');
+      bookDiv.classList.add('book');
+
+      bookDiv.innerHTML = `
+      <p><strong>Title:</strong> ${book.title}</p>
+      <p><strong>Author:</strong> ${book.author}</p>
+      <p><strong>Pages:</strong> ${book.pages}</p>
+      <p><strong>Read:</strong> ${book.read ? "Yes" : "No"}</p>
+      <button onclick="removeBook(${index})">Remove</button>
+        `;
+      libraryEl.appendChild(bookDiv);
+
+    });
+  }
 }
 function display() {
   let libraryEl = document.querySelector(".library");
@@ -24,14 +58,6 @@ function display() {
     let bookDiv = document.createElement("div");
     bookDiv.classList.add("book");
 
-    bookDiv.innerHTML = `
-      <p><strong>Title:</strong> ${book.title}</p>
-      <p><strong>Author:</strong> ${book.author}</p>
-      <p><strong>Pages:</strong> ${book.pages}</p>
-      <p><strong>Read:</strong> ${book.read ? "Yes" : "No"}</p>
-      <button onclick="removeBook(${index})">Remove</button>
-        `;
-    libraryEl.appendChild(bookDiv);
   })
 }
 function removeBook(index) {
@@ -40,12 +66,12 @@ function removeBook(index) {
 }
 
 let newBookbtn = document.querySelector('#new-book-btn');
-newBookbtn.addEventListener('click', function () {
+newBookbtn.addEventListener('click', function() {
   let bookForm = document.querySelector('#book-form');
   bookForm.style.display = "block"
 });
 
-document.querySelector('#add-book-btn').addEventListener("click", function (e) {
+document.querySelector('#add-book-btn').addEventListener("click", function(e) {
   e.preventDefault();
   addBookToLibrary();
 })
